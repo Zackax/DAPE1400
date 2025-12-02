81727fd66925f310a929b0e047b3560f583e225c
Zack <zackariasaxelson@gmail.com>
2025-12-02 14:53:06 +0100

    la til flere klasser

---
+++ EksamenH18/Dato.java
@@ -0,0 +1,23 @@
+package EksamenH18;
+
+class Dato {
+				private int dag,	måned,	år;
+				public Dato(	int dag,	int måned,	int år	)	{
+						this.dag =	dag;
+						this.måned =	måned;
+						this.år =år;
+				}
+				private static String	månedsnavn(	int mnd	)	{
+						String[]	navn	=	{	"januar",	"februar",	"mars",	"april",	"mai",	
+								"juni",	"juli",	"august",	"september",	"oktober",	"november",	
+								"desember" };
+						if (	mnd	>	0	&&	mnd	<	13	)
+								return navn[	mnd	- 1	];
+						else
+								return "ukjent	måned";
+				}
+				
+				//	skriv	en	toString	metode	slik	at	datoen	kommer	ut	på	følgende	format:
+				//	dd	månedsnavn	år,	f.eks	1	januar	2019
+				
+}
--- EksamenH18/Main.java
+++ EksamenH18/Main.java
@@ -3,6 +3,6 @@ package EksamenH18;
 public class Main {
 
   public static void main(String[] args) {
-    
+    int [] helTallsListe = {-3,41,5,-3,2,49};
   }
 }
