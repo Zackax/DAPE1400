2a9c69de23eb6a6bbdb16b0b74c4a6445b5353b2
Zack <zackariasaxelson@gmail.com>
2025-12-02 14:47:07 +0100

    La til 3 nye filer for oppgaven

---
+++ EksamenH18/Ansatt.java
@@ -0,0 +1,10 @@
+package EksamenH18;
+
+class Ansatt{
+				//	opprett	3	attributter
+				//	navn,	født	og	tiltrådt,	de	to	siste	skal	være	av	type	Dato.
+				
+				//	opprett	også	en	konstruktør	for	disse
+			//	lag	også	en	toString-metode	for	klassens	attributter
+}
+
